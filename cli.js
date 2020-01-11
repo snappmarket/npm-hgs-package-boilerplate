89715aa967a62e5159d011077c88184234ef3832
mostafa rastegar <mostafa.rastegar@snapp.market>
2020-01-11 12:02:52 +0330

    Test generator

--- cli.js
+++ cli.js
@@ -3,44 +3,77 @@ var shell = require("shelljs");
 var readline = require('readline'),
   rl = readline.createInterface(process.stdin, process.stdout);
 const [,, ...args] = process.argv;
-const former = [
-  "build and prepare for set version (yes or no) ? ","set commit message: ","set new virsion: v"
-];
+
 let counter = 0;
 const pjson = require('./package.json');
 console.log(`current version is: v${pjson.version}`);
-const q1 = () =>{
+
+
+const former = [
+  "build and prepare for set version (yes or no) ? ","set commit message: ","set new virsion: v"
+];
+
+const buildAndSetVersion = (input) => {
+  if(input === 'no') return rl.close();
+  console.log(`npm run build && git add -A .`);
+  shell.exec(`npm run build && git add -A .`);
+  console.log(`done!`);
+  return counter++;
+  // q1();
+}
+
+const commitMessage = (input) => {
+  console.log(`git commit -m '${input}'`);
+  shell.exec(`git commit -m '${input}'`);
+  console.log(`done!`);
+  return counter++;
+  // q1();
+}
+
+const setNewVersion = (input) => {
+  shell.exec(`npm version ${input}`);
+  if(input === pjson.version) {
+    return counter--
+  }else{
+    console.log(`done!`);
+    console.log(`please type 'npm run publisher'`);
+    return counter++;
+  }
+  // q1();
+}
+
+const q1 = (func) =>{
+  console.log(counter)
   if(counter > former.length - 1){
     rl.close();
     return true;
   }
+  
   rl.question(former[counter], (input) => {
-    switch(counter){
-      case 0:
-        if(input === 'no') return rl.close();
-        console.log(`npm run build && git add -A .`);
-        shell.exec(`npm run build && git add -A .`);
-        console.log(`done!`);
-        break;
-        case 1:
-          console.log(`git commit -m '${input}'`);
-          shell.exec(`git commit -m '${input}'`);
-          console.log(`done!`);
-        break;
-      case 2:
-        
-        shell.exec(`npm version ${input}`);
-        if(input === pjson.version) {
-          counter--
-        }else{
-          console.log(`done!`);
-          console.log(`please type 'npm run publisher'`);
-        }
-
-        break;
-    }
-    counter++;
-    q1();
+    return func(input);
   });
 }
-q1();
+
+function* anotherGenerator() {
+  yield q1(buildAndSetVersion);
+  yield q1(commitMessage);
+  yield q1(setNewVersion);
+}
+
+function* anotherGenerator() {
+  yield setTimeout(()=>console.log(1),100);
+  yield console.log(2);
+  yield console.log(3);
+}
+
+var gen = anotherGenerator();
+gen.next();
+gen.next();
+gen.next();
+
+// console.log(gen.next().value); // 0
+// console.log(gen.next().value); // 1
+// console.log(gen.next().value); // 2
+// console.log(gen.next().value); // 3
+
+// q1();
